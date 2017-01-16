98ee80ed0c8e7e6a8a43f136c8cfc94f070c6d65
tamal <tamal@appscode.com>
2017-01-16 12:12:55 -0800

    Generate new ci api.

---
+++ src/generated/main/grpc/com/appscode/api/ci/v1beta1/AgentsGrpc.java
@@ -0,0 +1,315 @@
+package com.appscode.api.ci.v1beta1;
+
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.0.2)",
+    comments = "Source: appscode/api/ci/v1beta1/agent.proto")
+public class AgentsGrpc {
+
+  private AgentsGrpc() {}
+
+  public static final String SERVICE_NAME = "appscode.ci.v1beta1.Agents";
+
+  // Static method descriptors that strictly reflect the proto.
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.appscode.api.dtypes.VoidRequest,
+      com.appscode.api.ci.v1beta1.AgentListResponse> METHOD_LIST =
+      io.grpc.MethodDescriptor.create(
+          io.grpc.MethodDescriptor.MethodType.UNARY,
+          generateFullMethodName(
+              "appscode.ci.v1beta1.Agents", "List"),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.dtypes.VoidRequest.getDefaultInstance()),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.ci.v1beta1.AgentListResponse.getDefaultInstance()));
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.appscode.api.ci.v1beta1.AgentCreateRequest,
+      com.appscode.api.ci.v1beta1.AgentCreateResponse> METHOD_CREATE =
+      io.grpc.MethodDescriptor.create(
+          io.grpc.MethodDescriptor.MethodType.UNARY,
+          generateFullMethodName(
+              "appscode.ci.v1beta1.Agents", "Create"),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.ci.v1beta1.AgentCreateRequest.getDefaultInstance()),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.ci.v1beta1.AgentCreateResponse.getDefaultInstance()));
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.appscode.api.ci.v1beta1.AgentDeleteRequest,
+      com.appscode.api.dtypes.VoidResponse> METHOD_DELETE =
+      io.grpc.MethodDescriptor.create(
+          io.grpc.MethodDescriptor.MethodType.UNARY,
+          generateFullMethodName(
+              "appscode.ci.v1beta1.Agents", "Delete"),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.ci.v1beta1.AgentDeleteRequest.getDefaultInstance()),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.dtypes.VoidResponse.getDefaultInstance()));
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static AgentsStub newStub(io.grpc.Channel channel) {
+    return new AgentsStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static AgentsBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new AgentsBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary and streaming output calls on the service
+   */
+  public static AgentsFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new AgentsFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class AgentsImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void list(com.appscode.api.dtypes.VoidRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.AgentListResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_LIST, responseObserver);
+    }
+
+    /**
+     */
+    public void create(com.appscode.api.ci.v1beta1.AgentCreateRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.AgentCreateResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_CREATE, responseObserver);
+    }
+
+    /**
+     */
+    public void delete(com.appscode.api.ci.v1beta1.AgentDeleteRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.dtypes.VoidResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_DELETE, responseObserver);
+    }
+
+    @java.lang.Override public io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            METHOD_LIST,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.appscode.api.dtypes.VoidRequest,
+                com.appscode.api.ci.v1beta1.AgentListResponse>(
+                  this, METHODID_LIST)))
+          .addMethod(
+            METHOD_CREATE,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.appscode.api.ci.v1beta1.AgentCreateRequest,
+                com.appscode.api.ci.v1beta1.AgentCreateResponse>(
+                  this, METHODID_CREATE)))
+          .addMethod(
+            METHOD_DELETE,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.appscode.api.ci.v1beta1.AgentDeleteRequest,
+                com.appscode.api.dtypes.VoidResponse>(
+                  this, METHODID_DELETE)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class AgentsStub extends io.grpc.stub.AbstractStub<AgentsStub> {
+    private AgentsStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private AgentsStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected AgentsStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new AgentsStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void list(com.appscode.api.dtypes.VoidRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.AgentListResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_LIST, getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void create(com.appscode.api.ci.v1beta1.AgentCreateRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.AgentCreateResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_CREATE, getCallOptions()), request, responseObserver);
+    }
+
+    /**
+     */
+    public void delete(com.appscode.api.ci.v1beta1.AgentDeleteRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.dtypes.VoidResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_DELETE, getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class AgentsBlockingStub extends io.grpc.stub.AbstractStub<AgentsBlockingStub> {
+    private AgentsBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private AgentsBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected AgentsBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new AgentsBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.appscode.api.ci.v1beta1.AgentListResponse list(com.appscode.api.dtypes.VoidRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_LIST, getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.appscode.api.ci.v1beta1.AgentCreateResponse create(com.appscode.api.ci.v1beta1.AgentCreateRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_CREATE, getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.appscode.api.dtypes.VoidResponse delete(com.appscode.api.ci.v1beta1.AgentDeleteRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_DELETE, getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class AgentsFutureStub extends io.grpc.stub.AbstractStub<AgentsFutureStub> {
+    private AgentsFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private AgentsFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected AgentsFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new AgentsFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.appscode.api.ci.v1beta1.AgentListResponse> list(
+        com.appscode.api.dtypes.VoidRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_LIST, getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.appscode.api.ci.v1beta1.AgentCreateResponse> create(
+        com.appscode.api.ci.v1beta1.AgentCreateRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_CREATE, getCallOptions()), request);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.appscode.api.dtypes.VoidResponse> delete(
+        com.appscode.api.ci.v1beta1.AgentDeleteRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_DELETE, getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_LIST = 0;
+  private static final int METHODID_CREATE = 1;
+  private static final int METHODID_DELETE = 2;
+
+  private static class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AgentsImplBase serviceImpl;
+    private final int methodId;
+
+    public MethodHandlers(AgentsImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_LIST:
+          serviceImpl.list((com.appscode.api.dtypes.VoidRequest) request,
+              (io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.AgentListResponse>) responseObserver);
+          break;
+        case METHODID_CREATE:
+          serviceImpl.create((com.appscode.api.ci.v1beta1.AgentCreateRequest) request,
+              (io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.AgentCreateResponse>) responseObserver);
+          break;
+        case METHODID_DELETE:
+          serviceImpl.delete((com.appscode.api.ci.v1beta1.AgentDeleteRequest) request,
+              (io.grpc.stub.StreamObserver<com.appscode.api.dtypes.VoidResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    return new io.grpc.ServiceDescriptor(SERVICE_NAME,
+        METHOD_LIST,
+        METHOD_CREATE,
+        METHOD_DELETE);
+  }
+
+}
---
+++ src/generated/main/grpc/com/appscode/api/ci/v1beta1/MetadataGrpc.java
@@ -0,0 +1,211 @@
+package com.appscode.api.ci.v1beta1;
+
+import static io.grpc.stub.ClientCalls.asyncUnaryCall;
+import static io.grpc.stub.ClientCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ClientCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ClientCalls.blockingUnaryCall;
+import static io.grpc.stub.ClientCalls.blockingServerStreamingCall;
+import static io.grpc.stub.ClientCalls.futureUnaryCall;
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+import static io.grpc.stub.ServerCalls.asyncUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncServerStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncClientStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncBidiStreamingCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+import static io.grpc.stub.ServerCalls.asyncUnimplementedStreamingCall;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.0.2)",
+    comments = "Source: appscode/api/ci/v1beta1/metadata.proto")
+public class MetadataGrpc {
+
+  private MetadataGrpc() {}
+
+  public static final String SERVICE_NAME = "appscode.ci.v1beta1.Metadata";
+
+  // Static method descriptors that strictly reflect the proto.
+  @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
+  public static final io.grpc.MethodDescriptor<com.appscode.api.dtypes.VoidRequest,
+      com.appscode.api.ci.v1beta1.ServerInfoResponse> METHOD_SERVER_INFO =
+      io.grpc.MethodDescriptor.create(
+          io.grpc.MethodDescriptor.MethodType.UNARY,
+          generateFullMethodName(
+              "appscode.ci.v1beta1.Metadata", "ServerInfo"),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.dtypes.VoidRequest.getDefaultInstance()),
+          io.grpc.protobuf.ProtoUtils.marshaller(com.appscode.api.ci.v1beta1.ServerInfoResponse.getDefaultInstance()));
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static MetadataStub newStub(io.grpc.Channel channel) {
+    return new MetadataStub(channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static MetadataBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    return new MetadataBlockingStub(channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary and streaming output calls on the service
+   */
+  public static MetadataFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    return new MetadataFutureStub(channel);
+  }
+
+  /**
+   */
+  public static abstract class MetadataImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void serverInfo(com.appscode.api.dtypes.VoidRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.ServerInfoResponse> responseObserver) {
+      asyncUnimplementedUnaryCall(METHOD_SERVER_INFO, responseObserver);
+    }
+
+    @java.lang.Override public io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            METHOD_SERVER_INFO,
+            asyncUnaryCall(
+              new MethodHandlers<
+                com.appscode.api.dtypes.VoidRequest,
+                com.appscode.api.ci.v1beta1.ServerInfoResponse>(
+                  this, METHODID_SERVER_INFO)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class MetadataStub extends io.grpc.stub.AbstractStub<MetadataStub> {
+    private MetadataStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private MetadataStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MetadataStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new MetadataStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void serverInfo(com.appscode.api.dtypes.VoidRequest request,
+        io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.ServerInfoResponse> responseObserver) {
+      asyncUnaryCall(
+          getChannel().newCall(METHOD_SERVER_INFO, getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class MetadataBlockingStub extends io.grpc.stub.AbstractStub<MetadataBlockingStub> {
+    private MetadataBlockingStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private MetadataBlockingStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MetadataBlockingStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new MetadataBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.appscode.api.ci.v1beta1.ServerInfoResponse serverInfo(com.appscode.api.dtypes.VoidRequest request) {
+      return blockingUnaryCall(
+          getChannel(), METHOD_SERVER_INFO, getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class MetadataFutureStub extends io.grpc.stub.AbstractStub<MetadataFutureStub> {
+    private MetadataFutureStub(io.grpc.Channel channel) {
+      super(channel);
+    }
+
+    private MetadataFutureStub(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected MetadataFutureStub build(io.grpc.Channel channel,
+        io.grpc.CallOptions callOptions) {
+      return new MetadataFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<com.appscode.api.ci.v1beta1.ServerInfoResponse> serverInfo(
+        com.appscode.api.dtypes.VoidRequest request) {
+      return futureUnaryCall(
+          getChannel().newCall(METHOD_SERVER_INFO, getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_SERVER_INFO = 0;
+
+  private static class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final MetadataImplBase serviceImpl;
+    private final int methodId;
+
+    public MethodHandlers(MetadataImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_SERVER_INFO:
+          serviceImpl.serverInfo((com.appscode.api.dtypes.VoidRequest) request,
+              (io.grpc.stub.StreamObserver<com.appscode.api.ci.v1beta1.ServerInfoResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    return new io.grpc.ServiceDescriptor(SERVICE_NAME,
+        METHOD_SERVER_INFO);
+  }
+
+}
